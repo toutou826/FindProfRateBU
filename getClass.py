4903062667075cb424e08ed98f416950161f1db5
toutou826 <37494307+toutou826@users.noreply.github.com>
2019-11-08 21:27:08 -0500

    Init
    read classes from a file
    get information about the class

---
+++ getClass.py
@@ -0,0 +1,27 @@
+import requests
+from bs4 import BeautifulSoup
+
+#Input name of the class, find and return section,teacher and time from catalog.
+def getClass(name):
+    result = {}
+
+    #Get response from the name of the class
+    url = "http://www.bu.edu/phpbin/course-search/section/"
+    querystring = {"t": name}
+    response = requests.request("POST", url, params=querystring)
+
+    soup = BeautifulSoup(response.text, "html.parser")
+    classList = soup.find_all("td")
+    divided = divide(classList, 8)
+    #Append section, teacher and time to result
+    for lst in divided:
+        if lst[0].get_text()[-1] == "1":
+            result[name + lst[0].get_text()] = (lst[2].get_text(), lst[5].get_text())
+    return result
+
+#Divide an array of sub array of size n
+def divide(arr, n):
+    result = []
+    for i in range(0, len(arr), n):
+        result.append(arr[i:i + n])
+    return result
